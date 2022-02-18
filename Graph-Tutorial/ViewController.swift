b2bb722382163109ae3420ed1df263fb01a9b61f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-02-18 12:35:46 -0800

    graph tutorial project created and initial graph added

--- Graph-Tutorial/ViewController.swift
+++ Graph-Tutorial/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
+    
+    var lineChart = LineChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        lineChart.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        lineChart.center = view.center
+        
+        view.addSubview(lineChart)
+        
+        var temperatures = [ChartDataEntry]()
+        var humidity = [ChartDataEntry]()
+        
+        for x in 0..<50 {
+            temperatures.append(ChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        
+        for x in 0...20 {
+            humidity.append(ChartDataEntry(x: Double(x + 10), y: Double(x)))
+        }
+        
+        let set = LineChartDataSet(entries: temperatures, label: "Temperature")
+        set.setColor(.systemRed)
+        set.drawCirclesEnabled = false
+//        set.colors = ChartColorTemplates.material()
+//        set.colors = ChartColorTemplates.
+        
+        let set2 = LineChartDataSet(entries: humidity, label: "Humidity")
+        set2.setColor(.systemBlue)
+        set2.drawCirclesEnabled = false
+        
+        let data = LineChartData(dataSets: [set,set2])
+        
+        lineChart.data = data
+        
     }
 
 
